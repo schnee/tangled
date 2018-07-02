a362a5e190c7538a37c6ff96ac11543a5b4dedd2
Brent Schneeman <brent.schneeman@dosh.cash>
2018-07-02 08:12:29 -0500

    commenting the state check code

--- tangled.R
+++ tangled.R
@@ -11,8 +11,12 @@ library(lubridate)
 
 tangled <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSosbIjCD2KyWJCm712HsEHCkSOdR75Gba5DbobZxlgNSeHjNutef7KkNHRiPU861sA10RfJwyQujuK/pub?gid=0&single=true&output=csv")
 
-
-
+# check the version of the data found on disk and compare it to the
+# docs.google.com version if they are identical, then there's nothing to do, so
+# bail out. If they are identical and maybe you feel that they should not be, it
+# could be because the Google CDN hasn't updated.
+#
+# you can skip over this code in interactive mode
 old_state_fn <- "./data/tangled.csv"
 if(file.exists(old_state_fn)){
   old_state <- read_csv(old_state_fn)

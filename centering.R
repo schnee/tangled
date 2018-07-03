75d36111a92a6d4a0e87d31b8071ae9ab98be46d
Brent Schneeman <brent.schneeman@dosh.cash>
2018-07-03 14:53:39 -0500

    initial prototyping on the layout for a bump chart view.

---
+++ centering.R
@@ -0,0 +1,39 @@
+# prototyping on how to "center" the top of a list and surround it by nearest
+# descencents fanning out
+
+df <- tibble(
+  index = c(1,2,3,4,5,6),
+  text = c("one", "two", "three", "four", "five", "six")
+)
+
+arrange_centered <- function(df) {
+  odd_half <- df %>% filter((row_number() + 1)  %% 2 == 0)
+  even_half <- df %>% filter(row_number()  %% 2 == 0) %>% arrange(-row_number())
+  
+  bind_rows(even_half, odd_half)
+}
+df
+df %>% arrange_centered()
+
+# determine the "centered" order of the groups based on their max centrality
+# score
+group_order <- max_cent %>% arrange(desc(centrality)) %>% arrange_centered()
+
+# extract all the nodes, and factorize the "group", ordering by the centered
+# order
+node_df <- g %>% activate(nodes) %>% as_tibble()
+node_df$group <- factor(x = node_df$group,levels = group_order$group )
+
+# group the nodes by group, and arrange the /grouped/ centralities. THEN created
+# a bunch on centered group data frames, which will be bind_rowed coming up
+centered_groups <- node_df %>% group_by(group) %>% arrange(desc(centrality), .by_group=TRUE) %>% do(
+  centered_group = arrange_centered(.)
+)
+
+centered_groups[2,]$centered_group
+
+# this has all the nodes in a by-group centering, with the most central group in
+# the middle, and the most central node in the middle of that. This should form
+# the basis of the bump chart
+the_ordered_nodes <- bind_rows(centered_groups$centered_group)
+

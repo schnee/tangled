5900d278d04664cc639c58d2a1e86c2902b9f5bc
Brent Schneeman <brent.schneeman@dosh.cash>
2018-12-06 12:23:00 -0600

    attempting to fix the line color issue

--- tangled.R
+++ tangled.R
@@ -52,7 +52,7 @@ the_layout <- create_layout(graph, layout = "igraph", algorithm = "drl", options
 
 
 p <- ggraph(the_layout ) +
-  geom_edge_fan(aes(linetype=e_type, color = e_type, 
+  geom_edge_fan(aes(edge_linetype=e_type, edge_color = e_type, 
                     label=note), edge_width=.5,
                 end_cap=circle(3,"mm"), spread = 3, start_cap = circle(3,"mm"), 
                 label_dodge = unit(2,"mm"), label_size = 2,
@@ -60,9 +60,9 @@ p <- ggraph(the_layout ) +
   scale_edge_linetype_manual(guide = "none", values=c(5, rep(1, length(the_edge_types) -1))) +
   scale_edge_color_brewer(name="Relationship", type="qual", palette = "Dark2") +
   geom_node_point(color = "black", size = 4.5) +
-  geom_node_point(aes(colour = group_label),size = 3.5) + 
+  geom_node_point(aes(colour = group_label),size = 3.5) +
   geom_node_point(color = "white", size = 1)+
-  geom_node_label( aes(label=name, alpha = the_alpha), size=2, repel = TRUE) + 
+  geom_node_label( aes(label=name, alpha = the_alpha), size=2, repel = TRUE) +
   scale_alpha(range = c(0.1,0.75)) +
   scale_color_manual(name = "Community", values = my_pal) +
   ggthemes::theme_few() +
